09163d29481032a2c51c68d63034a30f74c85054
Kinh Pham <giakinh451@gmail.com>
2022-07-27 13:25:53 +0700

    Update Config

---
+++ config/shopify.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ngrok' => env('NGROK' , 'https://abc8-113-161-32-170.ap.ngrok.io'),
+    'shopify_api_key' => env('SHOPIFY_API_KEY', 'b0579adb53c4c6f9ec27248709d62afb'),
+    'shopify_secret_key' => env('SHOPIFY_SECRET_KEY', 'bcb759044e7b631293b5fe922a2eb30d'),
+    'shopify_domain' => env('SHOPIFY_DOMAIN','https://huskadian2.myshopify.com'),
+    'shopify_access_token' => env('SHOPIFY_ACCESS_TOKEN','shpua_132e2aa272cd702b9c053538b101a360'),
+];
