74da35f6bd665eecae648791e964f2ca427654c8
phamnhatkhanh <khanhhcm4@gmail.com>
2022-07-31 23:03:07 +0700

    DB: additional code to Model

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Campaign_Process;
 class Campaign extends Model
 {
     use HasFactory;
+
+    protected $connection = 'mysql_campaigns';
     protected $table = 'campaigns';
+    
     protected $fillable = [
         'store_id',
         'name',
@@ -16,4 +19,8 @@ class Campaign extends Model
         'content',
         'footer'
     ];
+    public function store()
+    {
+    	return $this->belongsTo(Campaign_Process::class);
+    }
 }
