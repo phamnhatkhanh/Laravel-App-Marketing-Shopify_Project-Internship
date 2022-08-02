7283d0fe549740cadd796d9e31d80aa2a29ae4a8
phamnhatkhanh <khanhhcm4@gmail.com>
2022-08-02 13:28:37 +0700

    Shopify: setup archiecture code

--- app/Http/Controllers/Shopify/ShopifyController.php
+++ app/Http/Controllers/Shopify/ShopifyController.php
@@ -79,44 +79,115 @@ class ShopifyController extends Controller
         // return redirect()->route('login');
         return redirect('http://127.0.0.1:8000/api/dashboard');
     }
-
-    //Đăng kí ProductWebhooks thêm, xóa, sửa
-    public function registerProductWebhook($shop, $access_token)
+    public static function getAccessToken(string $code, string $domain)
     {
-        RegisterCustomerWebhookService::registerProductWebhookService($shop, $access_token);
+        $client2 = new Client();
+        $response = $client2->post(
+            "https://" . $domain . "/admin/oauth/access_token",
+            [
+                'form_params' => [
+                    'client_id' => env('SHOPIFY_API_KEY'),
+                    'client_secret' => env('SHOPIFY_SECRET_KEY'),
+                    'code' => $code,
+                ]
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents());
     }
 
-    //Đưa vào Queue để lưu những khách hàng đã được tạo trên Shopify vào DB
-    public static function createFromShopify($payload)
-    {
-        $data =  dispatch(new CreateCustomer($payload));
+    //Lấy thông tin đăng nhập
+    public static function getDataLogin($shop, $access_token){
+        $url = 'https://' . $shop . '/admin/api/2022-07/shop.json?';
+        $client = new Client();
+        $dataAuthen = $client->request('GET', $url, [
+            'headers' => [
+                'X-Shopify-Access-Token' => $access_token,
+            ]
+        ]);
+        $getDataStore = (array)json_decode($dataAuthen->getBody());
 
-        return response([
-            'data' => $data,
-            'status' => 201
-        ], 201);
+        return $getDataStore;
     }
 
-    //Đưa vào Queue để tự động lưu những khách hàng đã được sửa trên Shopify vào DB
-    public static function updateFromShopify($payload)
+    //Lấy thông tin khách hàng từ Shopify về
+    public static function createDataCustomer($shop, $access_token)
     {
-       $data =  dispatch(new UpdateCustomer($payload));
+        $client = new Client();
+        $url = 'https://' . $shop . '/admin/api/2022-07/customers.json';
+        $resProduct = $client->request('get', $url, [
+            'headers' => [
+                'X-Shopify-Access-Token' => $access_token,
+            ]
+        ]);
 
-       return response([
-        'data' => $data,
-        'status' => 201
-    ], 201);
+        return (array)json_decode($resProduct->getBody()->getContents());
     }
-
-    //Đưa vào Queue để tự động xóa khách hàng đã xóa trên Shopify trong DB
-    public static function deleteFromShopify($payload)
+    //Lưu thông tin Shopify
+    public static function saveDataLogin($res, $access_token)
     {
-        $data = dispatch(new DeleteCustomer($payload));
+        $saveData = $res['shop'];
 
-        return response([
-            'data' => $data,
-            'status' => 201
-        ], 201);
+        $findCreateAT = array('T', '+07:00');
+        $replaceCreateAT = array(' ', '');
+        $findUpdateAT = array('T', '+07:00');
+        $replaceUpdateAT = array(' ', '');
+
+        $created_at = str_replace($findCreateAT, $replaceCreateAT, $saveData->created_at);
+        $updated_at = str_replace($findUpdateAT, $replaceUpdateAT, $saveData->updated_at);
+
+        $password = Session::get('password');
+
+        $dataPost = [
+            'id' => $saveData->id,
+            'name_merchant' => $saveData->name,
+            'domain' => $saveData->domain,
+            'myshopify_domain' => $saveData->domain,
+            'email' => $saveData->email,
+            'phone' => $saveData->phone,
+            'password' => $password['password'],
+            'access_token' => $access_token,
+            'plan_name' => $saveData->plan_name,
+            'address' => $saveData->address1,
+            'zip' => $saveData->zip,
+            'city' => $saveData->city,
+            'country_name' => $saveData->country_name,
+            'created_at' => $created_at,
+            'updated_at' => $updated_at,
+        ];
+        Store::create($dataPost);
+
+        return $dataPost;
+    }
+    //Lưu khách hàng vào DB
+    public static function saveDataCustomer($getCustomer)
+    {
+        $saveCustomers = $getCustomer['customers'];
+        info($saveCustomers);
+        $findCreateAT = array('T', '+07:00');
+        $replaceCreateAT = array(' ', '');
+
+        $findUpdateAT = array('T', '+07:00');
+        $replaceUpdateAT = array(' ', '');
+        // $store_id = Session::get('store_id');
+        foreach ($saveCustomers as $customer) {
+            $created_at = str_replace($findCreateAT, $replaceCreateAT, $customer->created_at);
+            $updated_at = str_replace($findUpdateAT, $replaceUpdateAT, $customer->updated_at);
+
+            Customer::create([
+                'id' => $customer->id,
+                'store_id'  => 65147142383,
+                'email' => $customer->email,
+                'first_name' => $customer->first_name,
+                'last_name' => $customer->last_name,
+                'orders_count' => $customer->orders_count,
+                'total_spent' => $customer->total_spent,
+                'phone' => '574-805-7422',
+                // 'phone' => $customer->phone,
+                // 'created_at' => $created_at,
+                // 'updated_at' => $updated_at,
+            ]);
+        }
     }
 
 }
--- app/Http/Controllers/Shopify/WebhookController.php
+++ app/Http/Controllers/Shopify/WebhookController.php
@@ -11,7 +11,7 @@ class WebhookController extends Controller
     {
         $topic = $request->header('X-Shopify-Topic');
         $payload = $request->all();
-        
+
         switch ($topic) {
             case 'customers/update':
                 //Update data Product
@@ -28,4 +28,79 @@ class WebhookController extends Controller
                 ShopifyController::deleteFromShopify($payload);
         }
     }
+
+    //Đăng kí ProductWebhooks thêm, xóa, sửa
+    public function registerProductWebhook($shop, $access_token)
+    {
+        RegisterCustomerWebhookService::registerProductWebhookService($shop, $access_token);
+    }
+ public function accessPermission(){
+        //  GetProducts::dispatch();
+        $access_permission =array(
+            "app/uninstalled",
+            "products/create",
+            "products/update",
+            "products/delete" ,
+        );
+        //   dd(env("DOMAIN_NGROK"));
+        $store_id  = Session::get('store_id');
+        $store_token = Session::get('store_token');
+        $store = Store::where('id',$store_id)->first();
+        $url = "https://". $store->myshopify_domain ."/admin/api/2022-07/webhooks.json";
+        foreach ($access_permission as $access) {
+
+            $topic_webhook = array(
+                "webhook"=> array(
+                    "topic"=> $access,
+                    "address"=>env("DOMAIN_NGROK")."/api/webhook/".$access,
+                    "format"=>"json",
+                )
+            );
+
+            $getDataProduct =  Http::withHeaders([
+                'X-Shopify-Access-Token' =>  $store->app_token,
+            ])->withBody(json_encode($topic_webhook), 'application/json')->post($url);
+        }
+
+        // After install app: Sync all product have store merchant.
+        return redirect("/admin/list-product");
+
+
+    }
+
+    //Đưa vào Queue để lưu những khách hàng đã được tạo trên Shopify vào DB
+    public static function createFromShopify($payload)
+    {
+        $data =  dispatch(new CreateCustomer($payload));
+
+        return response([
+            'data' => $data,
+            'status' => 201
+        ], 201);
+    }
+
+    //Đưa vào Queue để tự động lưu những khách hàng đã được sửa trên Shopify vào DB
+    public static function updateFromShopify($payload)
+    {
+       $data =  dispatch(new UpdateCustomer($payload));
+
+       return response([
+        'data' => $data,
+        'status' => 201
+    ], 201);
+    }
+
+    //Đưa vào Queue để tự động xóa khách hàng đã xóa trên Shopify trong DB
+    public static function deleteFromShopify($payload)
+    {
+        $data = dispatch(new DeleteCustomer($payload));
+
+        return response([
+            'data' => $data,
+            'status' => 201
+        ], 201);
+    }
+
+
 }
+
