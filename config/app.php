3514fcd1dc67ae92079cb47ee7297fc51104684c
phamnhatkhanh <khanhhcm4@gmail.com>
2022-08-15 09:44:11 +0700

    Package: remove package not essential

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         App\Providers\GoogleDriveServiceProvider::class,
 
     ],
