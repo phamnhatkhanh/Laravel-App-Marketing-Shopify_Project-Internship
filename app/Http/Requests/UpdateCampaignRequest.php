4f3c607bdbfb1cb97e1d5e66dc31d2ca67a4159a
root <giakinh451@gmail.com>
2022-08-24 10:25:49 +0000

    Validate Campaign

--- app/Http/Requests/UpdateCampaignRequest.php
+++ app/Http/Requests/UpdateCampaignRequest.php
@@ -13,7 +13,7 @@ class UpdateCampaignRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,26 @@ class UpdateCampaignRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'subject' => 'required',
+            'content' => 'required',
+            'footer' => 'required|max:200',
+            'background_banner' => 'required|image|mimes:jpeg,png,jpg,gif|max:5120',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Campaign name cannot be empty.',
+            'name.max' => 'Campaign name cannot exceed 255 characters.',
+            'subject.required' => 'Campaign subject cannot be empty.',
+            'content.required' => 'Campaign content cannot be empty.',
+            'footer.required' => 'Campaign footer cannot be empty.',
+            'footer.max' => 'Campaign footer cannot exceed 255 characters.',
+            'background_banner.image' => 'Must be an image.',
+            'background_banner.mimes' => 'Must be the format: jpeg, png, jpg, gif.',
+            'background_banner.max' => 'Campaign image cannot exceed 5MB.',
         ];
     }
 }
--- app/Http/Requests/UpdateCustomerRequest.php
+++ app/Http/Requests/UpdateCustomerRequest.php
@@ -27,4 +27,5 @@ class UpdateCustomerRequest extends FormRequest
             //
         ];
     }
+
 }
